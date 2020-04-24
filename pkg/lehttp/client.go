f236bb7bdcebee1736d771cc24601010bbb29bf5
Ilya Shtrikul <shtricul@gmail.com>
2020-04-24 16:08:38 +0300

    added jsonRPC client constructor

--- pkg/lehttp/client.go
+++ pkg/lehttp/client.go
@@ -1,10 +1,14 @@
 package lehttp
 
 import (
+	"emperror.dev/emperror"
+	"emperror.dev/errors"
+	"github.com/go-kit/kit/transport/http/jsonrpc"
 	"github.com/go-resty/resty/v2"
 	"github.com/shurcooL/graphql"
 	"go.opencensus.io/plugin/ochttp"
 	"net/http"
+	"net/url"
 )
 
 func NewClient() *http.Client {
@@ -19,4 +23,13 @@ func NewRestyClient(host string) *resty.Client {
 
 func NewGraphQLClient(endpoint string) *graphql.Client {
 	return graphql.NewClient(endpoint, NewClient())
+}
+
+func NewJsonRPCClient(addr, method string, option ...jsonrpc.ClientOption) *jsonrpc.Client {
+	u, err := url.Parse(addr)
+	emperror.Panic(errors.Wrap(err, "can't create JsonRPC client"))
+
+	option = append([]jsonrpc.ClientOption{jsonrpc.SetClient(NewClient())}, option...)
+
+	return jsonrpc.NewClient(u, method, option...)
 }

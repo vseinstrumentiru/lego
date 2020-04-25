1509161e25ab18e8ec7baedb12e1180342bc5b21
Ilya Shtrikul <shtricul@gmail.com>
2020-04-25 22:14:40 +0300

    added client options

--- pkg/lehttp/client.go
+++ pkg/lehttp/client.go
@@ -1,6 +1,7 @@
 package lehttp
 
 import (
+	"crypto/tls"
 	"emperror.dev/emperror"
 	"emperror.dev/errors"
 	"github.com/go-kit/kit/transport/http/jsonrpc"
@@ -12,9 +13,53 @@ import (
 	"net/url"
 )
 
-func NewClient(name string) *http.Client {
+type ClientOption func(*http.Client)
+
+func SetSpanNameFormatter(formatter func(req *http.Request) string) ClientOption {
+	return func(client *http.Client) {
+		if octr, ok := client.Transport.(*ochttp.Transport); ok {
+			octr.FormatSpanName = formatter
+		}
+	}
+}
+
+func SetTraceSampler(sampler trace.Sampler) ClientOption {
+	return func(client *http.Client) {
+		if octr, ok := client.Transport.(*ochttp.Transport); ok {
+			octr.StartOptions.Sampler = sampler
+		}
+	}
+}
+
+func InsecureClient() ClientOption {
+	return func(client *http.Client) {
+		var httpTr *http.Transport
+		if octr, ok := client.Transport.(*ochttp.Transport); ok {
+			if octr.Base == nil {
+				octr.Base = http.DefaultTransport
+			}
+
+			httpTr = octr.Base.(*http.Transport)
+		} else if httpTr, ok = client.Transport.(*http.Transport); !ok {
+			return
+		}
+
+		if httpTr == nil {
+			return
+		}
+
+		if httpTr.TLSClientConfig == nil {
+			httpTr.TLSClientConfig = &tls.Config{}
+		}
+
+		httpTr.TLSClientConfig.InsecureSkipVerify = true
+	}
+}
+
+func NewClient(name string, opts ...ClientOption) *http.Client {
 	return &http.Client{
 		Transport: &ochttp.Transport{
+			Base: http.DefaultTransport,
 			StartOptions: trace.StartOptions{
 				Sampler: trace.AlwaysSample(),
 			},
@@ -25,19 +70,19 @@ func NewClient(name string) *http.Client {
 	}
 }
 
-func NewRestyClient(name string, host string) *resty.Client {
-	return resty.NewWithClient(NewClient(name)).SetHostURL(host)
+func NewRestyClient(name string, host string, opts ...ClientOption) *resty.Client {
+	return resty.NewWithClient(NewClient(name, opts...)).SetHostURL(host)
 }
 
-func NewGraphQLClient(name string, endpoint string) *graphql.Client {
+func NewGraphQLClient(name string, endpoint string, opts ...ClientOption) *graphql.Client {
 	return graphql.NewClient(endpoint, NewClient(name))
 }
 
-func NewJsonRPCClient(name string, addr, method string, option ...jsonrpc.ClientOption) *jsonrpc.Client {
+func NewJsonRPCClient(addr, method string, client *http.Client, option ...jsonrpc.ClientOption) *jsonrpc.Client {
 	u, err := url.Parse(addr)
 	emperror.Panic(errors.Wrap(err, "can't create JsonRPC client"))
 
-	option = append([]jsonrpc.ClientOption{jsonrpc.SetClient(NewClient(name))}, option...)
+	option = append([]jsonrpc.ClientOption{jsonrpc.SetClient(client)}, option...)
 
 	return jsonrpc.NewClient(u, method, option...)
 }

63140e02872de86dbb4905383b686890c2b40901
Ilya Shtrikul <shtricul@gmail.com>
2020-04-25 20:53:05 +0300

    fix trace

--- pkg/lehttp/client.go
+++ pkg/lehttp/client.go
@@ -7,29 +7,37 @@ import (
 	"github.com/go-resty/resty/v2"
 	"github.com/shurcooL/graphql"
 	"go.opencensus.io/plugin/ochttp"
+	"go.opencensus.io/trace"
 	"net/http"
 	"net/url"
 )
 
-func NewClient() *http.Client {
+func NewClient(name string) *http.Client {
 	return &http.Client{
-		Transport: &ochttp.Transport{},
+		Transport: &ochttp.Transport{
+			StartOptions: trace.StartOptions{
+				Sampler: trace.AlwaysSample(),
+			},
+			FormatSpanName: func(req *http.Request) string {
+				return name + ":" + req.URL.Path
+			},
+		},
 	}
 }
 
-func NewRestyClient(host string) *resty.Client {
-	return resty.NewWithClient(NewClient()).SetHostURL(host)
+func NewRestyClient(name string, host string) *resty.Client {
+	return resty.NewWithClient(NewClient(name)).SetHostURL(host)
 }
 
-func NewGraphQLClient(endpoint string) *graphql.Client {
-	return graphql.NewClient(endpoint, NewClient())
+func NewGraphQLClient(name string, endpoint string) *graphql.Client {
+	return graphql.NewClient(endpoint, NewClient(name))
 }
 
-func NewJsonRPCClient(addr, method string, option ...jsonrpc.ClientOption) *jsonrpc.Client {
+func NewJsonRPCClient(name string, addr, method string, option ...jsonrpc.ClientOption) *jsonrpc.Client {
 	u, err := url.Parse(addr)
 	emperror.Panic(errors.Wrap(err, "can't create JsonRPC client"))
 
-	option = append([]jsonrpc.ClientOption{jsonrpc.SetClient(NewClient())}, option...)
+	option = append([]jsonrpc.ClientOption{jsonrpc.SetClient(NewClient(name))}, option...)
 
 	return jsonrpc.NewClient(u, method, option...)
 }
